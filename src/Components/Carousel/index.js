5dade25aea087a060b9945a7bbe327b0ab419861
MDPyuri <matsumoto.prado@aluno.ifsp.edu.br>
2024-09-03 08:45:29 -0300

    adjusting the Carrousel logic

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -1,41 +1,60 @@
 import React from "react";
-import { useEffect } from "react"
+import { useEffect } from "react";
 import "./style.css";
 
-
 const Carousel = () => {
+  function buttons() {
+    console.log('a')
+  }
 
-    useEffect(() => {
-        const carouselNum = 2
-        let actualCarousel = 1
-        let carouselAnim = setInterval(() => {
-            if (actualCarousel === carouselNum) {
-                actualCarousel = 1
-                document.getElementById('carouselPicture1').classList.remove('outsideCarousel')
-                document.getElementById('carouselPicture1').classList.add('carouselPicture')
-                document.getElementById('carouselPicture2').classList.add('outsideCarousel')
-                document.getElementById('carouselPicture2').classList.remove('carouselPicture')
-            } else {
-                actualCarousel++
-                document.getElementById('carouselPicture1').classList.add('outsideCarousel')
-                document.getElementById('carouselPicture1').classList.remove('carouselPicture')
-                document.getElementById('carouselPicture2').classList.remove('outsideCarousel')
-                document.getElementById('carouselPicture2').classList.add('carouselPicture')
-            }
-
-        }, 5000);
-    })
-
-    return (
-        <div className="carousel">
-            <div className="carouselPicture picture1" id="carouselPicture1">
-
-            </div>
-            <div className="outsideCarousel testPicture" id="carouselPicture2">
-                <h1>Teste Carrossel</h1>
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    const carouselNum = 2;
+    let actualCarousel = 1;
+    let carouselAnim = setInterval(() => {
+      if (actualCarousel === carouselNum) {
+        actualCarousel = 1;
+        document
+          .getElementById("carouselPicture1")
+          .classList.remove("outsideCarousel");
+        document
+          .getElementById("carouselPicture1")
+          .classList.add("carouselPicture");
+        document
+          .getElementById("carouselPicture2")
+          .classList.add("outsideCarousel");
+        document
+          .getElementById("carouselPicture2")
+          .classList.remove("carouselPicture");
+      } else {
+        actualCarousel++;
+        document
+          .getElementById("carouselPicture1")
+          .classList.add("outsideCarousel");
+        document
+          .getElementById("carouselPicture1")
+          .classList.remove("carouselPicture");
+        document
+          .getElementById("carouselPicture2")
+          .classList.remove("outsideCarousel");
+        document
+          .getElementById("carouselPicture2")
+          .classList.add("carouselPicture");
+      }
+    }, 5000);
+  });
+  return (
+    <div className="carousel">
+      <div
+        className="carouselButtons previous"
+        id="previousCarousel"
+        onClick={buttons}></div>
+      <div className="carouselPicture picture1" id="carouselPicture1"></div>
+      <div className="outsideCarousel testPicture" id="carouselPicture2">
+        <h1>Teste Carrossel</h1>
+      </div>
+      <div className="carouselButtons next" id="nextCarousel"></div>
+    </div>
+  );
+};
 
 export default Carousel;
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className='textFooter'>
         <p className='license'>© 2024 Todos Direitos Reservados a Mariane</p>
       </div>
     </footer>
