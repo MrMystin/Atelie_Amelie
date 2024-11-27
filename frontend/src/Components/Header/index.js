ae775946db2d84c79fb8ccddbf0ccb113e12d8b4
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2024-11-27 11:49:51 -0300

    Starting New UI

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/logo/fg-G1.png';
 
 const Header = () => {
   return (
